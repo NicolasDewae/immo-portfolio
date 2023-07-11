01d321606f4404d571e7b4b0f89bc64b74c6980b
Nicolas De waegenaere <de_waegenaere@live.fr>
2023-07-11 12:14:03 +0200

    price updated

--- src/component/price/Price.jsx
+++ src/component/price/Price.jsx
@@ -16,9 +16,9 @@ const Price = () => {
                     <h2>{priceComponent.title}</h2>
                     <p>{price.priceBase} € TTC</p>
                     <p>{priceComponent.nbPhoto} photos</p>
-                    <p>livrées en {priceComponent.time}</p>
+                    <p>Livrées en {priceComponent.time}</p>
                     <div className="detailPrice">
-                        <p>Pour toutes photos supplémentaire, le prix sera de {price.extraPhoto} € par photo. il faut compter entre 30 minutes et 1h30 en fonction du bien</p>
+                        <p>Pour toute photo supplémentaire, le prix sera de {price.extraPhoto} € par photo.</p>
                     </div>
                 </div>
                 <div className="hiddenImg">
